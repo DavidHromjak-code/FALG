4a52d8ddf8e565bec3c60878a9fb4e967d3c40a0
Midnight77 <57727696+Midnight77@users.noreply.github.com>
2023-03-17 11:58:57 +0100

    UKOL 1 expanded loading data quarry

--- pythonServer/loadData.py
+++ pythonServer/loadData.py
@@ -1,6 +1,7 @@
 # import required modules
 import requests
 from domain.anime import Anime
+from domain.anime import Param
 
 # base_url variable to store url
 url = "https://api.jikan.moe/v4/anime/"
@@ -11,7 +12,22 @@ url = "https://api.jikan.moe/v4/anime/"
 
 # get method of requests module
 # return response object
-response = requests.get(url)
+
+sfw = input("Want only sfw results true/false?")
+
+order_by = input('Order by "mal_id" "title" "type" "rating" "start_date" "end_date" "episodes" "score" "scored_by" "rank" "popularity" "members" "favorites" ?')
+
+minScore = None
+if order_by == "score":
+    minScore = input("Select minimal score for anime form 1 - 9:")
+
+sort = input("sort asc or desc?")
+
+paramObject = Param(sfw,order_by,minScore,sort)
+
+print("This is param object: " + paramObject.__str__())
+
+response = requests.get(url, params= {"sfw" : paramObject.sfw,"sort": paramObject.sort, "order_by": paramObject.order_by, "min_score" : paramObject.minScore})
 
 # json method of response object
 # convert json format data into
@@ -40,5 +56,5 @@ if dataAsJson["data"]:
 else:
     print(" Api not working ")
 
- 
+    print(" Jinej kok branch ")
 
