2e3143e02d541ff9f7bf02af2351dce537067fce
David <davidhromjak259@gmail.com>
2023-05-10 19:42:22 +0200

    Added support of ell

--- pythonServer/web/script.js
+++ pythonServer/web/script.js
@@ -1,14 +1,12 @@
  async function get_data_python() {
    eel.get_data()((setData) => {
        setData.forEach(object => {
-           console.log(object);
            object = JSON.parse(object);
            console.log(object);
-         const para = document.createElement("p");
-         const node = document.createTextNode(object["engName"]);
-         para.appendChild(node);
-         const element = document.getElementById("content");
-         element.appendChild(para);
+           const para = document.createElement("div");
+           para.innerHTML = `<h1>${object["engName"]}</h1>`
+           const element = document.getElementById("content");
+           element.appendChild(para);
        })
      }
    )
